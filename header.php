e340a419a56928f49b3c8dd2463261c6cb77d2ad
SplitTv <53311923+SplitTv@users.noreply.github.com>
2019-07-25 19:00:03 +0200

    Add files via upload

---
+++ header.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#main-navbar" aria-controls="main-navbar" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <a class="navbar-brand" href="index.php"> <?php echo($title) ?></a>
+
+  <div class="collapse navbar-collapse" id="main-navbar">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="index.php">Startseite</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="?p=forum/index ">Foren</a>
+      </li>
+    </ul>
+      <ul class="navbar-nav">
+        <li class="nav-item pull-right">
+          <a class="nav-link" href="?p=login">Anmelden</a>
+      </li>
+      <li class="nav-item pull-right">
+        <a class="nav-link" href="?p=login">Registrieren</a>
+    </ul>
+  </div>
+</nav>
